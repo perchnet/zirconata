bf9f76a6cc467274a2a63d6caca56cff6409c633
Arthur Exaltação <arthur.exaltacao@chainguard.dev>
2025-12-08 13:39:51 -0300

    feat: enable autoupdates with restart
    Signed-off-by: Arthur Exaltação <arthur.exaltacao@chainguard.dev>

--- build.sh
+++ build.sh
@@ -72,11 +72,11 @@ dnf -y install --setopt=install_weak_deps=False \
 systemctl enable NetworkManager
 systemctl enable systemd-timesyncd
 
-sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
 sed -i 's|^OnUnitInactiveSec=.*|OnUnitInactiveSec=7d\nPersistent=true|' /usr/lib/systemd/system/bootc-fetch-apply-updates.timer
 sed -i 's|#AutomaticUpdatePolicy.*|AutomaticUpdatePolicy=stage|' /etc/rpm-ostreed.conf
 sed -i 's|#LockLayering.*|LockLayering=true|' /etc/rpm-ostreed.conf
 
+# sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
 systemctl enable bootc-fetch-apply-updates
 
 tee /usr/lib/systemd/zram-generator.conf <<'EOF'
