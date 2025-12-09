60165d3f83293dfbbd466a47dba6356e34b7043b
Tulip Blossom <tulilirockz@outlook.com>
2025-12-08 22:48:24 -0300

    fix: enable bootc-fetch-apply by default
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- build.sh
+++ build.sh
@@ -56,8 +56,7 @@ sed -i 's|^OnUnitInactiveSec=.*|OnUnitInactiveSec=7d\nPersistent=true|' /usr/lib
 sed -i 's|#AutomaticUpdatePolicy.*|AutomaticUpdatePolicy=stage|' /etc/rpm-ostreed.conf
 sed -i 's|#LockLayering.*|LockLayering=true|' /etc/rpm-ostreed.conf
 
-# sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
-systemctl enable bootc-fetch-apply-updates
+systemctl enable bootc-fetch-apply-updates.timer
 
 tee /usr/lib/systemd/zram-generator.conf <<'EOF'
 [zram0]
